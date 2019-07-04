f042cc18b88d0a5c8293e4c88ce71c8549db8070
Gülnur <gulnur.umurr@gmail.com>
2019-07-04 12:56:58 +0300

    ilkyorum

---
+++ lib/email_attachments.dart/email.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_web/material.dart';
+
+class Mail extends StatefulWidget {
+  @override
+  _MailState createState() => _MailState();
+}
+
+class _MailState extends State<Mail> {
+  String attachment;
+
+  final _recipientController =TextEditingController(
+    text: 'example@example.com',
+  );
+
+  final _subjectController =TextEditingController(
+    text: 'The subject'
+  );
+ //merhaba 
+  final _bodyController=TextEditingController(
+    text: 'Mail body'
+  );
+
+  final GlobalKey<ScaffoldState> _scaffoldKey=GlobalKey<ScaffoldState>();
+
+  Future<void> send() async {
+    final Email email =Email(
+      body: _bodyController
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
